15b6339d5c86c74c52b17cd1dfb758afeac925ec
Justin <alexanderrr416@gmail.com>
2023-11-24 21:31:52 -0500

    Create sub service module for fetching data.

--- app/services/sub-service.ts
+++ app/services/sub-service.ts
@@ -5,14 +5,30 @@ export interface Subscriber {
   name: string;
   username: string;
   email: string;
-  city: string;
   phone: string;
+  address: { city: string };
+  company: { name: string };
 }
 
 class SubService {
   async getAllSubs(): Promise<Subscriber[]> {
     const res = await fetch(baseUrl + "/users");
-    return res.json();
+    const subs = await res.json();
+    return subs;
+  }
+
+  async deleteSub(id: number): Promise<void> {
+    try {
+      const res = await fetch(baseUrl + `/usexrs/${id}`, {
+        method: "DELETE",
+      });
+
+      if (!res.ok) {
+        throw new Error(`Failed to delete subscriber. Status: ${res.status}`);
+      }
+    } catch (err) {
+      throw new Error(`${(err as Error).message}`);
+    }
   }
 }
 
